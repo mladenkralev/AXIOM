9d997c53940966c652b4d55460bd030a1f5e433c
Mladen Kralev <Mladen.Kralev@softwareag.com>
2017-11-01 11:19:43 +0200

    Adding test axis service creator

---
+++ axis2-service/src/main/java/com/softwareag/wsstack/VersionService.java
@@ -0,0 +1,12 @@
+package com.softwareag.wsstack;
+
+public class VersionService {
+	
+    public String getVersion() throws Exception {
+       return "PASTA";
+    }
+    
+    public String getPlainVersion() {
+        return "PASTAAPASTAAA";
+    }
+}
